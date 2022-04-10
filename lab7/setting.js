77cea9feb85004b8c6b948f0dd67f4f15d59e416
TanyaEmka <tanyaemelyanova2015@gmail.com>
2022-04-11 02:05:43 +0300

    lab7 add other info

--- lab7/setting.js
+++ lab7/setting.js
@@ -1,6 +1,9 @@
 const input_text = document.getElementById("search_text");
 const header = document.getElementById("header");
 const tempr = document.getElementById("temp");
+const temp_desc = document.getElementById("desc");
+const icon = document.getElementById("icon");
+const other = document.getElementById("other");
 let city_name = "Москва";
 let apiKey = "5005c41ae2aa1d2711fdd7359a984e81";
 let city_id = 0;
@@ -10,13 +13,20 @@ function getWeather(e) {
         city_name = input_text.value;
         header.textContent = city_name;
     }
-    fetch("https://api.openweathermap.org/data/2.5/find?q=" + city_name + ",RU&type=like&APPID=" + apiKey)
+    fetch("https://api.openweathermap.org/data/2.5/find?q=" + city_name + "&lang=ru&type=like&APPID=" + apiKey)
     .then(function(res) {
         return res.json();
     })
     .then(function(data) {
         let temperature = data["list"][0]["main"].temp;
-        tempr.textContent = (Math.round(temperature - 273)).toString() + String.fromCharCode(176);
+        let weather_text = (Math.round(temperature - 273)).toString() + String.fromCharCode(176);
+        temp_desc.textContent = data["list"][0]["weather"][0].description;
+        console.log(temp_desc.textContent);
+        tempr.textContent = weather_text;
+        console.log(data);
+        console.log("https://openweathermap.org/img/wn/" + data["list"][0]["weather"][0].icon + "@2x.png");
+        icon.src = "https://openweathermap.org/img/wn/" + data["list"][0]["weather"][0].icon + "@2x.png";
+        other.textContent = Math.round(data["list"][0]["wind"].speed) + "   м/c";
         console.log(data);
     });
 }
